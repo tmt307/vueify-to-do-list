26fed9c9a4577f8a738bc70d92658a22cc967981
tmt307 <turnertm02@gmail.com>
2019-09-05 16:13:25 +0100

    First Batch Upload
    Everything works but edit button on the app.

---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,106 @@
+<template>
+    <v-row>
+        <v-col cols="12" sm="6" offset-sm="3">
+            <v-card>
+
+                <v-toolbar color="blue darken-4">
+                    <v-toolbar-title class="white--text">  {{title}}</v-toolbar-title>
+                    <div class="flex-grow-1"></div>
+
+
+                    <template v-slot:extension>
+                        <v-btn fab
+                               color="blue darken-1"
+                               bottom
+                               left
+                               absolute
+                               @click="dialog = !dialog">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </template>
+                </v-toolbar>
+
+                <v-list>
+                    <v-list-item v-for="todo in todolist">
+                        <v-list-item-content>
+                            <v-list-item-title :class="{ done: todo.done}"> {{todo.title}} </v-list-item-title>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                            <v-btn class=" mr-4" color=" green accent-4" dark>
+                                <v-checkbox class="mr-2" type="checkbox" v-model="todo.done"></v-checkbox>
+                                Completed
+                            </v-btn>
+                        </v-list-item-action>
+
+                        <v-list-item-action>
+                            <v-btn  @click="dialog = !dialog" color="gray accent-4" dark>
+                                <v-icon 
+                                 >mdi-pencil </v-icon> <span class="m-2">Edit</span>
+                            </v-btn>
+                        </v-list-item-action>
+
+                        <v-list-item-action @click="deletetodo(todo)" >
+                            <v-btn color="red accent-4" dark>
+                                <v-icon dark>mdi-delete-circle </v-icon> <span class="m-2">Delete</span>
+                            </v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list>
+
+                <v-dialog v-model="dialog" max-width="500px">
+                    <v-card>
+                        <v-card-actions>
+                            <div class="flex-grow-1">
+                                <form @submit.prevent="todoformaddmethod">
+
+                                    <v-text-field required v-model="newtodo" type="text" name="newtodo" value="2" id="newtodo" label="Task Name"></v-text-field>
+
+
+                                    <v-btn color="primary" name="button" type="submit">add</v-btn>
+
+
+                                    <v-btn color="red" @click="dialog = false">  Close</v-btn>
+                                </form>
+                            </div>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<style>
+    .done {
+        text-decoration: line-through
+    }
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+                dialog: false,
+                title: 'To do list',
+                newtodo: '',
+                todolist: []
+
+            }
+        },
+        methods: {
+            todoformaddmethod() {
+                this.todolist.push({
+                    title: this.newtodo,
+                    done: false
+                });
+                this.newtodo = '';
+            },
+            deletetodo(todo) {
+                const todoIndex = this.todolist.indexOf(todo);
+                this.todolist.splice(todoIndex, 1);
+            }
+        }
+    }
+
+</script>
---
+++ my-app/src/components/todoapp.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import todoapp from './components/todoapp';
+
+export default {
+  name: 'App',
+  components: {
+    todoapp,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
